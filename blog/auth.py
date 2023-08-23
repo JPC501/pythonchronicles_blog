546947fe061415cb11c53e8c4d0b544ba8533bb3
Joan-Perez <kalvyn501@gmail.com>
2023-08-23 16:20:07 -0700

    finished forms login and register

--- blog/auth.py
+++ blog/auth.py
@@ -2,6 +2,12 @@ from flask import Blueprint, render_template
 
 bp_auth = Blueprint('auth', __name__, url_prefix='/auth')
 
+#login form
 @bp_auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('auth_templates/login.html')
+    return render_template('auth/login.html')
+
+#register form
+@bp_auth.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template('auth/register.html')
