672dd4c4a9b8a7ec3263ee10a547b455355a0f43
Joan Perez <117424143+JPC501@users.noreply.github.com>
2023-09-18 23:41:18 -0700

    using pep 8

--- blog/auth.py
+++ blog/auth.py
@@ -1,5 +1,5 @@
 from flask import (
-    Blueprint, render_template, request, 
+    Blueprint, render_template, request,
     redirect, url_for, flash, session, g
 )
 from .models.auth_register import User
@@ -7,6 +7,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 from .models.posts import Posts
 from werkzeug.utils import secure_filename
+import functools
 
 
 bp_auth = Blueprint('auth', __name__, url_prefix='/auth')
@@ -34,11 +35,11 @@ def register():
 
         else:
             if user_name_validator is not None:
-                error = f'El nombre de suario ya existe, elija otro!'
+                error = 'El nombre de suario ya existe, elija otro!'
                 flash(error)
 
             elif email_validator is not None:
-                error = f'El Email ya existe, elija otro!'
+                error = 'El Email ya existe, elija otro!'
                 flash(error)
 
     return render_template('authentication/register.html')
@@ -66,23 +67,24 @@ def login():
             session['user.id'] = user.id
 
             return redirect(url_for('main.index'))
-    
+
     return render_template('authentication/login.html', error=error)
 
-#keep user logged
+# keep user logged
+
 
 @bp_auth.before_app_request
 def load_user():
     user_id = session.get('user.id')
 
-    if user_id == None:
+    if user_id is None:
         g.user = None
     else:
         g.user = User.query.get_or_404(user_id)
-        
 
-import functools
-#protecting the views
+
+# protecting the views
+
 
 def login_required(view):
     @functools.wraps(view)
@@ -92,7 +94,9 @@ def login_required(view):
         return view(**kwargs)
     return wrapped_view
 
-#show profile 
+# show profile
+
+
 @bp_auth.route('/profile', methods=['GET'])
 @login_required
 def profile():
@@ -100,10 +104,12 @@ def profile():
     photo = get_picture(id)
     posts = Posts.query.filter_by(author=g.user.id).all()
     post_count = Posts.query.filter_by(author=g.user.id).count()
-    
-    return render_template('authentication/profile.html', 
-                            user=user, photo=photo, 
-                            posts=posts, post_count=post_count,)
+
+    return render_template(
+        'authentication/profile.html',
+        user=user, photo=photo,
+        posts=posts, post_count=post_count,
+    )
 
 
 # edit profile
@@ -129,7 +135,9 @@ def edit_profile(id):
 
             if request.files['image']:
                 photo = request.files['image']
-                photo.save(f'blog/static/media/{secure_filename(photo.filename)}')
+                photo.save(
+                    f'blog/static/media/{secure_filename(photo.filename)}'
+                )
                 user.photo = f'media/{secure_filename(photo.filename)}'
 
             if name and name != "":
@@ -151,13 +159,10 @@ def get_picture(id):
     user = User.query.get_or_404(g.user.id)
     return user.photo if user.photo is not None else None
 
-        
+
 # close session
 
 @bp_auth.route('/logout')
 def logout():
     session.clear()
     return redirect(url_for('main.index'))
-    
-    
-
