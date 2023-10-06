8910960d0e1582387a284471d02b64e37c011954
Joan Perez <117424143+JPC501@users.noreply.github.com>
2023-10-05 23:07:16 -0700

    Implement category-specific templates

--- blog/categories/categories_views.py
+++ blog/categories/categories_views.py
@@ -1,7 +1,6 @@
-from flask import(
-    Blueprint, render_template,
-    redirect, g, flash, url_for
-    
+from flask import (
+    Blueprint, render_template
+
 )
 
 bp_categories = Blueprint('categories', __name__, url_prefix='/categories')
@@ -9,6 +8,16 @@ bp_categories = Blueprint('categories', __name__, url_prefix='/categories')
 
 #  show routes of categories
 
-@bp_categories.route('/Backend', methods=['GET', 'POST'])
+@bp_categories.route('/backend', methods=['GET', 'POST'])
 def backend():
     return render_template('categories/backend.html')
+
+
+@bp_categories.route('data', methods=['GET', 'POST'])
+def data_analysis():
+    return render_template('categories/data.html')
+
+
+@bp_categories.route('gaming', methods=['GET', 'POST'])
+def gaming():
+    return render_template('categories/gaming.html')
