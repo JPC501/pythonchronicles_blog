5e00321c65f7ae84d5d901b9ed354f28c2d781b2
Alver J Perez <117424143+JPC501@users.noreply.github.com>
2023-11-30 20:46:35 -0800

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 
 class Config():
     SQLALCHEMY_DATABASE_URI = (
-        'postgresql://postgres:joan50%401$23@localhost/python-chronicles')
+        'postgresql://your-db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'dev'
     PERMANENT_SESSION_LIFETIME = timedelta(minutes=20)
